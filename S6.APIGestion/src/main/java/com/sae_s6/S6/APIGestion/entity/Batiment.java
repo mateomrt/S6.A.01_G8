d4a496cda2a16b795f2f92302810a8785b153bc8
berthem1 <berthem1@D250-S28>
2025-06-10 11:15:53 +0200

    batiment entity wip. build.gradle dependencies added.

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/Batiment.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/Batiment.java
@@ -1,5 +1,52 @@
 package com.sae_s6.S6.APIGestion.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "t_e_batiment_bat")
 public class Batiment {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bat_id")
+    private int id;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "bat_nom", nullable = false, length = 50)
+    private String nom;
+
+    @OneToMany(mappedBy = "batiment", cascade = CascadeType.ALL)
+    private List<Salle> salles = new ArrayList<>();
+
+    // Getters & setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Salle> getSalles() {
+        return salles;
+    }
+
+    public void setSalles(List<Salle> salles) {
+        this.salles = salles;
+    }
 }
