f274e7ccaadf581dac917ce4e36e2920038dc58b
NicolasTochon <nicolas.tochon@etu.univ-smb.fr>
2025-06-10 11:44:05 +0200

    Type_equipement entity

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/Type_Equipement.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/Type_Equipement.java
@@ -1,8 +1,12 @@
 package com.sae_s6.S6.APIGestion.entity;
 
+import java.util.List;
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,5 +23,21 @@ public class Type_Equipement {
     private Integer id_type_equipement;
 
     @Column(name = "libelle_type_equipement", nullable = false, length = 250)
-    private String titre;
+    private String libelle_type_equipement;
+
+    @OneToMany(mappedBy = "type_equipement")
+    private List<Capteur> capteurs;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Type_Equipement type_equipement)) return false;
+        return Objects.equals(id_type_equipement, type_equipement.id_type_equipement)
+                && Objects.equals(libelle_type_equipement, type_equipement.libelle_type_equipement);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_type_equipement, libelle_type_equipement);
+    }
 }
