3dffdbba01658bf73407472813b044b9faba0153
BastosT <bastien.taillez74@icloud.com>
2025-06-10 11:43:03 +0200

    service + repo salle

---
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/SalleRepo.java
@@ -0,0 +1,8 @@
+package com.sae_s6.S6.APIGestion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sae_s6.S6.APIGestion.entity.Salle;
+
+public interface SalleRepo extends JpaRepository<Salle, Integer>  {
+
+}
