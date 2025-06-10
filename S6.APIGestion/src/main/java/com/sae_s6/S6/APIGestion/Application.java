85a6101e9e7d598b1079a274ece3f5c2076b9e19
mateomrt <mateo.martin@etu.univ-smb.fr>
2025-06-10 09:18:55 +0200

    added spring boot

---
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/Application.java
@@ -0,0 +1,13 @@
+package com.sae_s6.S6.APIGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
