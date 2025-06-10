1d10f9cbf81b34900e176fce49ac710519fc32d5
BastosT <bastien.taillez74@icloud.com>
2025-06-10 11:23:50 +0200

    gg

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/Salle.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/Salle.java
@@ -1,68 +1,68 @@
-// package com.sae_s6.S6.APIGestion.entity;
+package com.sae_s6.S6.APIGestion.entity;
 
 
-// import java.util.List;
-// import java.util.Objects;
+import java.util.List;
+import java.util.Objects;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.JoinColumn;
-// import jakarta.persistence.ManyToOne;
-// import jakarta.persistence.OneToMany;
-// import jakarta.persistence.Table;
-// import lombok.AllArgsConstructor;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// @Entity
-// @Table(name = "salle")
-// @Data
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class Salle {
+@Entity
+@Table(name = "salle")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Salle {
 
 
-//     @Id
-//     private Integer id;
+    @Id
+    private Integer id;
 
-//     @Column(name = "titre", nullable = false, length = 250)
-//     private String titre;
+    @Column(name = "titre", nullable = false, length = 250)
+    private String titre;
 
-//     @Column(name = "superficie", nullable = false)
-//     private Double superficie;
+    @Column(name = "superficie", nullable = false)
+    private Double superficie;
 
-//     @ManyToOne
-//     @JoinColumn(name = "batiment_id", nullable = false)
-//     private Batiment batiment;
+    @ManyToOne
+    @JoinColumn(name = "batiment_id", nullable = false)
+    private Batiment batiment;
 
-//     @ManyToOne
-//     @JoinColumn(name = "typesalle_id", nullable = false)
-//     private TypeSalle typeSalle;
+    @ManyToOne
+    @JoinColumn(name = "typesalle_id", nullable = false)
+    private TypeSalle typeSalle;
 
-//     @OneToMany(mappedBy = "salle")
-//     private List<Mur> murs;
+    @OneToMany(mappedBy = "salle")
+    private List<Mur> murs;
 
-//     @OneToMany(mappedBy = "salle")
-//     private List<Equipement> equipements;
+    @OneToMany(mappedBy = "salle")
+    private List<Equipement> equipements;
 
-//     @OneToMany(mappedBy = "salle")
-//     private List<Capteur> capteurs;
+    @OneToMany(mappedBy = "salle")
+    private List<Capteur> capteurs;
 
-//     @Override
-//     public boolean equals(Object o) {
-//         if (this == o) return true;
-//         if (!(o instanceof Salle salle)) return false;
-//         return Objects.equals(id, salle.id)
-//                 && Objects.equals(titre, salle.titre)
-//                 && Objects.equals(superficie, salle.superficie)
-//                 && Objects.equals(batiment, salle.batiment)
-//                 && Objects.equals(typeSalle, salle.typeSalle);
-//     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Salle salle)) return false;
+        return Objects.equals(id, salle.id)
+                && Objects.equals(titre, salle.titre)
+                && Objects.equals(superficie, salle.superficie)
+                && Objects.equals(batiment, salle.batiment)
+                && Objects.equals(typeSalle, salle.typeSalle);
+    }
 
-//     @Override
-//     public int hashCode() {
-//         return Objects.hash(id, titre, superficie, batiment, typeSalle);
-//     }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titre, superficie, batiment, typeSalle);
+    }
 
-// }
+}
