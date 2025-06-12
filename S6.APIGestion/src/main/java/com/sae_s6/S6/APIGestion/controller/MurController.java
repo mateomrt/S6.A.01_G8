af19fd8296c0c9f302518ddc1f596241dc01186d
Oriven74 <plomionnathan@gmail.com>
2025-06-12 13:55:20 +0200

    feat: add CRUD operations for Mur entity in controller, service, and repository

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/controller/MurController.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/controller/MurController.java
@@ -1,5 +1,56 @@
 package com.sae_s6.S6.APIGestion.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.sae_s6.S6.APIGestion.entity.Mur;
+import com.sae_s6.S6.APIGestion.service.MurService;
+
+
+@RestController
+@RequestMapping("/murs")
 public class MurController {
     
+    private final MurService murService;
+
+    @Autowired
+    public MurController(MurService murService) {
+        this.murService = murService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Mur>> getAllMurs() {
+        return ResponseEntity.ok(murService.getAllMurs());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mur> getMurById(@PathVariable Integer id) {
+        return murService.getMurById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Mur> createMur(@RequestBody Mur mur) {
+        return ResponseEntity.ok(murService.createMur(mur));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mur> updateMur(@PathVariable Integer id, @RequestBody Mur updated) {
+        Mur updatedMur = murService.updateMur(id, updated);
+        if (updatedMur != null) {
+            return ResponseEntity.ok(updatedMur);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMur(@PathVariable Integer id) {
+        murService.deleteMur(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/MurRepo.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/MurRepo.java
@@ -1,5 +1,10 @@
 package com.sae_s6.S6.APIGestion.repository;
 
-public class MurRepo {
-    
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sae_s6.S6.APIGestion.entity.Mur;
+
+@Repository
+public interface MurRepo extends JpaRepository<Mur, Integer> {
+        
 }
