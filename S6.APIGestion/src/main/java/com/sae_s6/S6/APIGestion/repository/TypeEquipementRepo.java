f4cf8e70d2ce10ffade51af83450f6dc74c4c2bd
NicolasTochon <nicolas.tochon@etu.univ-smb.fr>
2025-06-12 15:11:16 +0200

    TypeEquipementService

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/TypeEquipementRepo.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/TypeEquipementRepo.java
@@ -2,8 +2,8 @@ package com.sae_s6.S6.APIGestion.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sae_s6.S6.APIGestion.entity.Equipement;
+import com.sae_s6.S6.APIGestion.entity.TypeEquipement;
 
-public interface TypeEquipementRepo extends JpaRepository<Equipement, Integer>{
+public interface TypeEquipementRepo extends JpaRepository<TypeEquipement, Integer>{
     
 }
