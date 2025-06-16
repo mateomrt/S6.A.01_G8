3ca03dfbc23e8734570eb8c8e68d91f6cd282a76
mateomrt <mateo.martin@etu.univ-smb.fr>
2025-06-16 10:56:07 +0200

    correctif

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/service/MurService.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/service/MurService.java
@@ -3,20 +3,20 @@ package com.sae_s6.S6.APIGestion.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.sae_s6.S6.APIGestion.entity.Mur;
 import com.sae_s6.S6.APIGestion.repository.MurRepo;
 
+import lombok.RequiredArgsConstructor;
+
 
 @Service
+@RequiredArgsConstructor
 public class MurService {
     private final MurRepo murRepo;
 
-    @Autowired
-    public MurService(MurRepo murRepo) {
-        this.murRepo = murRepo;
-    }
+    
 
     public List<Mur> getAllMurs() {
         return murRepo.findAll();
