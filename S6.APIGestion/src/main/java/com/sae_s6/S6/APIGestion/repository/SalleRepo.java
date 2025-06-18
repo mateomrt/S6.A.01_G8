aaee2c5a9302f49d702ebd0577e3a1aad3c9644f
Mateo MARTIN <martmate@iut-acy.local>
2025-06-18 09:00:28 +0200

    correctifs

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/SalleRepo.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/repository/SalleRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sae_s6.S6.APIGestion.entity.Salle;
 
 public interface SalleRepo extends JpaRepository<Salle, Integer>  {
-    List<Salle> findByTitre(String titre);
-    List<Salle> findByTitreLike(String titre);
+    List<Salle> findByLibelleSalle(String libelleSalle);
+    List<Salle> findByLibelleSalleLike(String libelleSalle);
 }
