10759f903d75cc99277a9b99204e2be8cbe0a148
Mateo MARTIN <martmate@iut-acy.local>
2025-06-19 10:05:52 +0200

    capteur

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/TypeCapteur.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/entity/TypeCapteur.java
@@ -48,4 +48,7 @@ public class TypeCapteur {
     public int hashCode() {
         return Objects.hash(id, libelleTypeCapteur, modeTypeCapteur);
     }
+    public String getDesc() {
+        return libelleTypeCapteur;
+    }
 }
