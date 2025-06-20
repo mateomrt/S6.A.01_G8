273a6009553866b748c6b9262143e8774cfb631c
Oriven74 <plomionnathan@gmail.com>
2025-06-20 15:01:07 +0200

    refonte view et editor typesalle meme raison

--- S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/views/TypeSalleView.java
+++ S6.APIGestion/src/main/java/com/sae_s6/S6/APIGestion/views/TypeSalleView.java
@@ -1,6 +1,5 @@
 package com.sae_s6.S6.APIGestion.views;
 
-
 import com.sae_s6.S6.APIGestion.entity.TypeSalle;
 import com.sae_s6.S6.APIGestion.service.TypeSalleService;
 import com.vaadin.flow.component.button.Button;
@@ -13,75 +12,74 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-@Route (value="typeSalle") 
+@Component
+@Scope("prototype")
+@Route(value = "typeSalle")
 @PageTitle("Les Types de salle")
-@Menu(title = "Les types de salle", order = 1, icon = "vaadin:clipboard-check")
-
+@Menu(title = "Les Types de salles", order = 1, icon = "vaadin:clipboard-check")
 public class TypeSalleView extends VerticalLayout {
 
-	//private final AuteurRepo repo;
-	private final TypeSalleService typeSalleService;
+    private final TypeSalleService typeSalleService;
 
-	final Grid<TypeSalle> grid;
+    public final Grid<TypeSalle> grid;
+    public final TextField filter;
+    private final Button addNewBtn;
+    public final TypeSalleEditor editor;
 
-	final TextField filter;
+    public Button getAddNewBtn() {
+        return addNewBtn;
+    }
 
-	private final Button addNewBtn;
+    public TypeSalleView(TypeSalleService typeSalleService, TypeSalleEditor editor) {
+        this.typeSalleService = typeSalleService;
+        this.editor = editor;
+        this.grid = new Grid<>(TypeSalle.class);
+        this.filter = new TextField();
+        this.addNewBtn = new Button("Ajouter un type de salle", VaadinIcon.PLUS.create());
 
-	//public AuteurView(AuteurRepo repo, AuteurEditor editor) {
-	public TypeSalleView(TypeSalleService typeSalleService, TypeSalleEditor editor) {
-		//this.repo = repo;
-		this.typeSalleService = typeSalleService;
-		//this.editor = editor;
-		this.grid = new Grid<>(TypeSalle.class);
-		this.filter = new TextField();
-		this.addNewBtn = new Button("Ajouter un type de salle", VaadinIcon.PLUS.create());
+        // Build layout
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        add(actions, grid, editor);
 
-		// build layout
-		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-		add(actions, grid, editor);
+        grid.setHeight("300px");
+        grid.setColumns("id", "libelleTypeSalle");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
 
-		grid.setHeight("300px");
-		grid.setColumns("id", "libelleTypeSalle");
-		
-		grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+        filter.setPlaceholder("Filtrer par nom");
 
-		filter.setPlaceholder("Filtrer par nom");
+        // Hook logic to components
 
-		// Hook logic to components
+        // Replace listing with filtered content when user changes filter
+        filter.setValueChangeMode(ValueChangeMode.LAZY);
+        filter.addValueChangeListener(e -> listTypeSalle(e.getValue()));
 
-		// Replace listing with filtered content when user changes filter
-		filter.setValueChangeMode(ValueChangeMode.LAZY);
-		filter.addValueChangeListener(e -> listTypeSalle(e.getValue()));
+        // Connect selected TypeSalle to editor or hide if none is selected
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            editor.editTypeSalle(e.getValue());
+        });
 
-		// Connect selected Customer to editor or hide if none is selected
-		grid.asSingleSelect().addValueChangeListener(e -> {
-			editor.editTypeSalle(e.getValue());
-		});
+        // Instantiate and edit new TypeSalle when the new button is clicked
+        addNewBtn.addClickListener(e -> editor.editTypeSalle(new TypeSalle()));
 
-		// Instantiate and edit new Customer the new button is clicked
-		addNewBtn.addClickListener(e -> editor.editTypeSalle(new TypeSalle()));
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listTypeSalle(filter.getValue());
+        });
 
-		// Listen changes made by the editor, refresh data from backend
-		editor.setChangeHandler(() -> {
-			editor.setVisible(false);
-			listTypeSalle(filter.getValue());
-		});
+        // Initialize listing
+        listTypeSalle(null);
+    }
 
-		// Initialize listing
-		listTypeSalle(null);
-	}
-
-	// tag::listTypeSalle[]
-	void listTypeSalle(String filterText) {
-		if (StringUtils.hasText(filterText)) {
-			grid.setItems(typeSalleService.getByLibelleTypeSalleContainingIgnoreCase(filterText));
-		} else {
-			grid.setItems(typeSalleService.getAllTypeSalles());
-		}
-	}
-	// end::listCustomers[]
-
+    void listTypeSalle(String filterText) {
+        if (StringUtils.hasText(filterText)) {
+            grid.setItems(typeSalleService.getByLibelleTypeSalleContainingIgnoreCase(filterText));
+        } else {
+            grid.setItems(typeSalleService.getAllTypeSalles());
+        }
+    }
 }
