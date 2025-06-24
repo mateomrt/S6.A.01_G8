a3c309ac8b5356baa11cfc554448376b40390f9e
NicolasTochon <nicolas.tochon@etu.univ-smb.fr>
2025-06-24 10:08:54 +0200

    TypeEquipementViewTest

--- S6.APIGestion/src/test/java/com/sae_s6/S6/APIGestion/view/TypeEquipementViewTest.java
+++ S6.APIGestion/src/test/java/com/sae_s6/S6/APIGestion/view/TypeEquipementViewTest.java
@@ -1,75 +1,82 @@
 package com.sae_s6.S6.APIGestion.view;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import com.sae_s6.S6.APIGestion.entity.TypeEquipement;
 import com.sae_s6.S6.APIGestion.views.TypeEquipementEditor;
 import com.sae_s6.S6.APIGestion.views.TypeEquipementView;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.data.provider.ListDataProvider;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Collection;
 
 @SpringBootTest
 public class TypeEquipementViewTest {
     @Autowired
     private TypeEquipementView typeEquipementView;
 
-    private Grid<TypeEquipement> grid;
-    private TypeEquipementEditor editor;
+    Logger logger = Logger.getLogger(TypeEquipementViewTest.class.getName());
 
-    @BeforeEach
-    void setUp() {
-        grid = typeEquipementView.grid;
-        editor = typeEquipementView.editor;
+    @Test
+    void testTypeEquipementViewLoaded() {
+       
+        assert typeEquipementView != null;
     }
 
     @Test
-    void typeEquipementViewLoaded() {
-        assertNotNull(typeEquipementView, "La vue TypeEquipementView doit être injectée");
+    void editorAfficheQuandTypeEquipementSelectionne() {
+        Grid<TypeEquipement> grid = typeEquipementView.grid;
+        TypeEquipement firstTypeEquipement = getFirstItem(grid);
+        logger.info("First TypeEquipement: " + firstTypeEquipement);
+
+        TypeEquipementEditor editor = typeEquipementView.editor;
+
+        assertFalse(editor.isVisible(), "Editor should not be visible initially");
+        grid.asSingleSelect().setValue(firstTypeEquipement);
+        assertTrue(editor.isVisible(), "Editor should be visible when a TypeEquipement is selected");
+
+        logger.info("TypeEquipement in editor: " + editor.libelleTypeEquipement.getValue());
+        assertEquals(firstTypeEquipement.getLibelleTypeEquipement(), editor.libelleTypeEquipement.getValue());
     }
 
     @Test
-    void editorInitialementCache() {
-        assertFalse(editor.isVisible(), "L'éditeur doit être caché au démarrage");
-    }
+    void editorAfficheQuandNewTypeEquipementClicked() {
+        TypeEquipementEditor editor = typeEquipementView.editor;
 
-    @Test
-    void clicAjouterAfficheEditeur() {
+        assertFalse(editor.isVisible(), "Editor should not be visible initially");
+
         typeEquipementView.getAddNewBtn().click();
-        assertTrue(editor.isVisible(), "L'éditeur doit s'afficher après clic sur 'Ajouter'");
+
+        assertTrue(editor.isVisible(), "Editor should be visible after clicking add new button");
+
+        assertEquals("", editor.libelleTypeEquipement.getValue(), "New TypeEquipement should have empty libelle");
+
+        // Simuler la saisie
+        editor.libelleTypeEquipement.setValue("Nouveau TypeEquipement Test");
+        editor.save.click();
+
+        // Vérifier que le nouveau TypeEquipement est bien dans la grille
+        TypeEquipement lastTypeEquipement = getLastItem(typeEquipementView.grid);
+        logger.info("New TypeEquipement should be last in grid: " + lastTypeEquipement);
+        assertEquals("Nouveau TypeEquipement Test", lastTypeEquipement.getLibelleTypeEquipement());
     }
 
-    @Test
-    void selectionDansGrilleAfficheEditeur() {
-        TypeEquipement typeEquipement = getFirstTypeEquipement();
-        grid.asSingleSelect().setValue(typeEquipement);
-        assertTrue(editor.isVisible(), "L'éditeur doit être visible après sélection");
-        assertEquals(typeEquipement.getLibelleTypeEquipement(), editor.libelleTypeEquipement.getValue());
+    private TypeEquipement getFirstItem(Grid<TypeEquipement> grid) {
+        return ((ListDataProvider<TypeEquipement>) grid.getDataProvider()).getItems().iterator().next();
     }
 
-    @Test
-    void colonnesDeGrilleCorrectes() {
-        assertNotNull(grid.getColumnByKey("id"));
-        assertNotNull(grid.getColumnByKey("libelleTypeEquipement"));
-    }
-
-    @Test
-    void champFiltrePresent() throws Exception {
-        var filterField = TypeEquipementView.class.getDeclaredField("filter");
-        filterField.setAccessible(true);
-        var textField = (com.vaadin.flow.component.textfield.TextField) filterField.get(typeEquipementView);
-        assertEquals("Filtrer par nom", textField.getPlaceholder());
-    }
-
-    private TypeEquipement getFirstTypeEquipement() {
+    private TypeEquipement getLastItem(Grid<TypeEquipement> grid) {
         Collection<TypeEquipement> typeEquipements = ((ListDataProvider<TypeEquipement>) grid.getDataProvider()).getItems();
-        return typeEquipements.iterator().next();
+        List<TypeEquipement> typeEquipementList = new ArrayList<>(typeEquipements);
+        return typeEquipementList.get(typeEquipementList.size() - 1);
     }
 
 }
